ada959eb5dbf4be72b5c9a9463853b8b66c7e34d
Jérémy Marodon <marodon.jeremy@gmail.com>
2019-12-13 15:26:19 +0100

    :construction_worker: Add CI process with PHPUnit/PHPStan/Code_Sniffer on TravisCI

---
+++ bootstrap-phpstan.php
@@ -0,0 +1,6 @@
+<?php
+
+// Without this we have an error locally
+// Constant T_DOC_COMMENT_STRING not found.
+// While it not present in TravisCI
+require_once './vendor/squizlabs/php_codesniffer/src/Util/Tokens.php';
